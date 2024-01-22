fbe2390a13bb79c9c574aba48b02936252fb2d34
SurabhiKeesara <surabhi.keesara@gmail>
2024-01-22 16:08:55 -0500

    Class activity part one, var and script errors

--- scripts/external.js
+++ scripts/external.js
@@ -1,4 +1,6 @@
 document.getElementById("demo").textContent = "A paragraph";
 function myFunction() {
+    // with .textContent, has to be a string
+    // with .innerHTML, just converts to string
     document.getElementById("demo").textContent = "Paragraph changed after click.";
 }
